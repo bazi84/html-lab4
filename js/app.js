4a716762b4e14a3103249cc2871c391ff41358d7
bazi84 <Abdulkadirsheikh03@gmail.com>
2021-06-30 15:29:37 -0700

    add js to html

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('Hello.World');
+
+let userName = 'Roger'
+let userAge = 45;
+
+console.log(userName)
+console.log(userAge)
+
+userAge =21
+console.log(userAge)
+
+userName = prompt ('Please Enter YOur name');
+console.log(userName)
+
+document.write('<h2>' + 'Welcome' + userName + '!</h2>')
---
+++ js/app2.js
@@ -0,0 +1,4 @@
+let likeDBZ = prompt('Do you like Dragonball Z?')
+if(likeDBZ == 'yes'){
+document.write('I like Dragonball Z')
+} else {document.write("I am sorry to hear that, Go back to spongy land")}
