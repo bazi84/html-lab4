e21e8fab186a4df32da3d0acb47862c935f9609d
bazi84 <Abdulkadirsheikh03@gmail.com>
2021-07-01 23:36:30 -0700

    add another function to html

--- js/app.js
+++ js/app.js
@@ -11,4 +11,47 @@ function getTime(){
     document.write('<h2>' + greeting + '!</h2>')
 }
 
+
+function doYouLikeDragonBallz(){
+    let LikeDragonBallz = prompt('Do you like Dragonball Z?')
+if(LikeDragonBallz == 'yes'){
+document.write('I like Dragonball Z')
+} else {document.write("I am sorry to hear that, Go back to spongy land")}
+
+}
+
+
+function getUserName(){
+    let user_name = prompt('Please Enter Your Name');
+    document.write(user_name + '!')
+}
+
+getUserName();
 getTime();
+doYouLikeDragonBallz();
+
+function guessingGamel(){
+    let correctAnswer = "Green";
+    let guessCount = 0;
+    let guessLimit = 3; 
+    let outOfGuesses = false
+    let win = false;
+
+    while(guessCount != 3 || win != true){
+        let userAnswer = prompt("Please guess what's my favorite color")
+        if(userAnswer == correctAnswer){
+            alert("Hooray you guessed correct")
+            win = true;
+            break;
+        } else {
+            alert("incorrect. Please guess again")
+            guessCount++
+            if(guessCount == guessLimit)
+            {alert('nice try!!! correct answer is Green')
+            break;}
+        }
+    }
+}
+guessingGamel()
+
+function myFunction( ) {var elmnt = document.getElementById("content"); elmnt.scrollIntoView();}
--- js/app3.js
+++
@@ -1,8 +0,0 @@
-function getUserName(){
-    let use_name = prompt('Please Enter Your Name');
-    document.write(user_name + '!')
-}
-
-getUserName();
-
-function myFunction( ) {var elmnt = document.getElementById("content"); elmnt.scrollIntoView();}
